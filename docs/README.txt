a72b6896d09475892c5cefc28c4ad01058224d85
Thierry Florac <thierry.florac@onf.fr>
2020-10-21 19:43:25 +0200

    Version 1.0.0

---
+++ docs/README.txt
@@ -0,0 +1,29 @@
+===================
+PyAMS JWT authentication package package
+===================
+
+.. contents::
+
+
+What is PyAMS?
+==============
+
+PyAMS (Pyramid Application Management Suite) is a small suite of packages written for applications
+and content management with the Pyramid framework.
+
+**PyAMS** is actually mainly used to manage web sites through content management applications (CMS,
+see PyAMS_content package), but many features are generic and can be used inside any kind of web
+application.
+
+All PyAMS documentation is available on `ReadTheDocs <https://pyams.readthedocs.io>`_; source code
+is available on `Gitlab <https://gitlab.com/pyams>`_ and pushed to `Github
+<https://github.com/py-ams>`_.
+
+
+What is PyAMS JWT authentication package?
+=========================================
+
+This package is a plug-in for PyAMS security policy; it allows to generate JWT tokens, and to
+extract and validate credentials from a provided JWT token.
+
+You have to include this package in your Pyramid configuration to active this plug-in.
---
+++ setup.py
@@ -0,0 +1,76 @@
+#
+# Copyright (c) 2015-2019 Thierry Florac <tflorac AT ulthar.net>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+
+"""
+This module contains PyAMS JWT authentication package package
+"""
+import os
+from setuptools import setup, find_packages
+
+
+DOCS = os.path.join(os.path.dirname(__file__),
+                    'docs')
+
+README = os.path.join(DOCS, 'README.txt')
+HISTORY = os.path.join(DOCS, 'HISTORY.txt')
+
+version = '1.0.0'
+long_description = open(README).read() + '\n\n' + open(HISTORY).read()
+
+tests_require = [
+    'pyams_site',
+    'pyramid_zodbconn'
+]
+
+
+setup(name='pyams_auth_jwt',
+      version=version,
+      description="PyAMS security plug-in for JWT authentication",
+      long_description=long_description,
+      classifiers=[
+          "License :: OSI Approved :: Zope Public License",
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='Pyramid PyAMS',
+      author='Thierry Florac',
+      author_email='tflorac@ulthar.net',
+      url='https://pyams.readthedocs.io',
+      license='ZPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=[],
+      include_package_data=True,
+      package_data={'': ['*.zcml', '*.txt', '*.pt', '*.pot', '*.po', '*.mo',
+                         '*.png', '*.gif', '*.jpeg', '*.jpg', '*.css', '*.js']},
+      zip_safe=False,
+      # uncomment this to be able to run tests with setup.py
+      test_suite="pyams_auth_jwt.tests.test_utilsdocs.test_suite",
+      tests_require=tests_require,
+      extras_require=dict(test=tests_require),
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'ZODB',
+          'persistent',
+          'pyams_security',
+          'pyams_utils >= 1.3.0',
+          'pyjwt[crypto]',
+          'pyramid',
+          'zope.container',
+          'zope.interface',
+          'zope.lifecycleevent',
+          'zope.schema'
+      ],
+      entry_points="")
