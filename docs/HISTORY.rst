0ff70558d0237fb05caaf2f788d4a956e2016b92
Thierry Florac <tflorac@ulthar.net>
2023-11-26 01:46:01 +0100

    Version 2.0.0

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -1,6 +1,10 @@
 Changelog
 =========
 
+2.0.0
+-----
+ - upgraded to Pyramid 2.0
+
 1.4.6
 -----
  - interfaces cleanup
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ DOCS = os.path.join(os.path.dirname(__file__),
 README = os.path.join(DOCS, 'README.rst')
 HISTORY = os.path.join(DOCS, 'HISTORY.rst')
 
-version = '1.4.6'
+version = '2.0.0'
 long_description = open(README).read() + '\n\n' + open(HISTORY).read()
 
 tests_require = [
@@ -73,7 +73,7 @@ setup(name='pyams_auth_jwt',
           'pyams_security',
           'pyams_utils >= 1.3.0',
           'pyjwt[crypto]',
-          'pyramid',
+          'pyramid >= 2.0.0',
           'requests',
           'zope.container',
           'zope.interface',
